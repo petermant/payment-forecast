a04a6f1aade3550b24d2fb5825997a2a13a0a98b
Pete Mantell <pete.mantell@version1.com>
2019-08-13 19:32:51 +0100

    Rationalized config slightly and added initial failing test for the webapp

---
+++ src/test/java/com/pete/payment/app/PaymentApplicationTests.java
@@ -0,0 +1,41 @@
+package com.pete.payment.app;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@TestPropertySource("classpath:test.properties")
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PaymentApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	private URL base;
+
+	@Autowired
+	private TestRestTemplate template;
+
+	@Before
+	public void beforePaymentApplicationTests() throws Exception {
+		this.base = new URL("http://localhost:" + port + "/forecast-summary");
+	}
+
+	@Test
+	public void forecastSummaryShouldReturnOK() throws Exception {
+		ResponseEntity<String> response = template.getForEntity(base.toString(), String.class);
+		assertEquals(200, response.getStatusCode().value());
+	}
+
+}
--- src/test/java/com/pete/payment/dataload/config/BatchTestConfig.java
+++ src/test/java/com/pete/payment/dataload/config/BatchTestConfig.java
@@ -14,21 +14,8 @@ import static org.mockito.Mockito.mock;
 public class BatchTestConfig {
 
     @Bean
-    protected DataSource dataSource() {
-        DriverManagerDataSource ds = new DriverManagerDataSource();
-        ds.setDriverClassName("org.h2.Driver");
-        ds.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE;");
-        return ds;
-    }
-
-    @Bean
     @Primary
     protected RowValidator rowValidator() {
         return mock(RowValidator.class);
     }
-
-//    @Bean
-//    protected JobLauncherTestUtils jobLauncherTestUtils() {
-//        return new JobLauncherTestUtils();
-//    }
 }
